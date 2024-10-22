870461e3db5a8812d2293adcc01b6c607c971bac
root <root@MMENYENE-IDIONG>
2024-10-22 09:33:04 +0100

    101-students.py101-students.py

--- 0x01-NoSQL/101-students.py101-students.py
+++ 0x01-NoSQL/101-students.py101-students.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+'''Task 14's module.
+'''
 
 
 def top_students(mongo_collection):
-    # sourcery skip: inline-immediately-returned-variable
-    """ Returns all students sorted by average score """
-    top_student = mongo_collection.aggregate([
-        {
-            "$project": {
-                "name": "$name",
-                "averageScore": {"$avg": "$topics.score"}
-            }
-        },
-        {"$sort": {"averageScore": -1}}
-    ])
-
-    return top_student
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
